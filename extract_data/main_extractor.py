2ecf37e8863c5e036b38b712c28ca05ad13664ce
kali <kali@kali.kali>
2025-11-18 11:21:04 -0500

    commit

--- extract_data/main_extractor.py
+++ extract_data/main_extractor.py
@@ -1,10 +1,10 @@
+# parser.add_argument("-d", "--debug", action="store_true", help="Print detailed logs")
+# parser.add_argument("-a", "--all", action="store_true", help="Include archived/disabled/private programs too")
+
 import json
 from extract_data.process import DataExtractor, CONFIG
-def get_extracet(debug: bool, 
-                 all: bool) -> str:
 
-    # parser.add_argument("-d", "--debug", action="store_true", help="Print detailed logs")
-    # parser.add_argument("-a", "--all", action="store_true", help="Include archived/disabled/private programs too")
+def get_extracet(debug: bool, all: bool) -> str:
 
     extractor = DataExtractor(config=CONFIG, include_all=all)
     results = extractor.extract()
@@ -14,6 +14,7 @@ def get_extracet(debug: bool,
     else:
         field_names = list(CONFIG["fields"].keys())
         output = []
+        
         if not debug:
             # pipeline (CSV-like)
             for p in results:
@@ -26,7 +27,8 @@ def get_extracet(debug: bool,
             for idx, p in enumerate(results, 1):
                 values = [f"{field}: {p.get(field, '')}" for field in field_names]
                 output.append(f"{idx:02d}. " + " — ".join(values))
-    if output:
-        return "".join(output)
-    else:
-        return "ERROR finaly"
+        
+        if output:
+            return "\n".join(output)
+        else:
+            return "ERROR finally"
