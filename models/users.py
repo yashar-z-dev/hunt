9ff9494bc5818ded8978de19a3e840419919b9e8
kali <kali@kali.kali>
2025-11-27 12:10:17 -0500

    fix return id as DB

--- models/users.py
+++ models/users.py
@@ -5,8 +5,12 @@ from models.db import DatabaseManager
 
 class User:
     """show one user datails"""
-    def __init__(self, chat_id: int, timestamp: Optional[str] = None, flags: str = "0000"):
-        self.id: Optional[int] = None  # Runtime get as DB
+    def __init__(self, 
+                 chat_id: int, 
+                 id: Optional[int] = None,
+                 timestamp: Optional[str] = None, 
+                 flags: str = "0000"):
+        self.id: Optional[int] = id
         self.chat_id = chat_id
         self.timestamp = timestamp or datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         self.flags = flags
@@ -28,13 +32,16 @@ class UserManager:
         query = "SELECT id, timestamp, chat_id, flags FROM users WHERE chat_id = ?"
         result = self.db_manager.fetch_one(query, (chat_id,))
         if result:
-            return User(chat_id=result[2], timestamp=result[1], flags=result[3])
+            return User(id=result[0] ,timestamp=result[1], chat_id=result[2], flags=result[3])
         return None
 
     def get_all_users(self) -> List[User]:
         query = "SELECT id, timestamp, chat_id, flags FROM users"
         results = self.db_manager.fetch_all(query)
-        return [User(chat_id=row[2], timestamp=row[1], flags=row[3]) for row in results]
+        all_users = []
+        for row in results:
+            all_users.append(User(id=row[0], timestamp=row[1], chat_id=row[2], flags=row[3]))
+        return all_users
 
     def update_flags(self, chat_id: int, new_flags: str):
         query = "UPDATE users SET flags = ? WHERE chat_id = ?"
